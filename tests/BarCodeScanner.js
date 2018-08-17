1c766ab41ca2b101ab27244ab81fe217bfdcad4a
Mateusz Maćkowski <m4tx@m4tx.pl>
2018-08-17 14:56:29 +0000

    Implement readFromURL() in BarCodeScanner (#2833)
    * [sdk] Add readFromURL() to BarCodeScanner
    
    * [sdk] Remove default barCodeTypes parameter for readFromURL
    
    * [sdk] Use proper barCodeTypes types on Android in readBarCodeFromURL()
    
    * [sdk] Small BarCodeReader.readFromURL() refactor
    
    * [ios] Use UIImage for ImageLoaderCompletionBlock
    
    * [android] Reformat BarCodeScannerModule.java
    
    * [sdk][android][ios] BarCodeScanner.readFromURL() returns multiple results, small refactor
    
    * [android] Fix ZXingBarCodeScanner returning null result
    
    * [sdk] Add BarCodeScanner tests
    
    * [docs] Add BarCodeScanner.readFromURL() docs
    
    * [sdk] Restore removed tests
    
    * [sdk] Fix null check in BarCodeScanner.readFromURL, fix formatting
    
    * [Android] Fix comment about return value in GMVBarCodeDetector
    
    * [sdk] Fix readFromURL function name in tests
    
    * [sdk] Use specific parameter type for readFromURL() in BarCodeScanner
    
    * [expo-image-loader-interface] Extract EXImageLoaderInterface unimodule
    
    * [shipit] Add shipit config for expo-image-loader-interface
    
    fbshipit-source-id: aedb17a

--- tests/BarCodeScanner.js
+++ tests/BarCodeScanner.js
@@ -1,7 +1,8 @@
 'use strict';
 
 import React from 'react';
-import { BarCodeScanner, Permissions } from 'expo';
+import { Platform } from 'react-native';
+import { Asset, BarCodeScanner, Permissions } from 'expo';
 import * as TestUtils from '../TestUtils';
 
 import { mountAndWaitFor as originalMountAndWaitFor } from './helpers';
@@ -38,6 +39,85 @@ export async function test(t, { setPortalChild, cleanupPortal }) {
     t.describe('when created', () => {
       t.it('displays the view', async () => {
         await mountAndWaitFor(<BarCodeScanner style={style} />);
+      });
+    });
+
+    t.describe('readFromURL', () => {
+      t.it('returns empty result when there is no barcode', async () => {
+        const asset = await Asset.fromModule(require('../assets/black-128x256.png'));
+        const result = await BarCodeScanner.readFromURL(asset.uri);
+
+        t.expect(result).toBeDefined();
+        t.expect(result.length).toEqual(0);
+      });
+
+      t.it('reads a QR code from asset', async () => {
+        const asset = await Asset.fromModule(require('../assets/qrcode_expo.jpg'));
+        const result = await BarCodeScanner.readFromURL(asset.uri);
+
+        t.expect(result).toBeDefined();
+        t.expect(result.length).toEqual(1);
+        t.expect(result[0]).toBeDefined();
+        t.expect(result[0].type).toEqual(BarCodeScanner.Constants.BarCodeType.qr);
+        t.expect(result[0].data).toEqual('https://expo.io/');
+      });
+
+      t.it('reads a QR code from photo asset', async () => {
+        // Public domain photo from https://commons.wikimedia.org/wiki/File:QR_Code_Damaged.jpg
+        const asset = await Asset.fromModule(require('../assets/qrcode_photo_wikipedia.jpg'));
+        const result = await BarCodeScanner.readFromURL(asset.uri);
+
+        t.expect(result).toBeDefined();
+        t.expect(result.length).toEqual(1);
+        t.expect(result[0]).toBeDefined();
+        t.expect(result[0].type).toEqual(BarCodeScanner.Constants.BarCodeType.qr);
+        t.expect(result[0].data).toEqual('http://en.m.wikipedia.org');
+      });
+
+      t.it('reads a QR code from base64 URL', async () => {
+        const url =
+          'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyAQMAAAAk8RryAAAABlBMVEX/' +
+          '//8AAABVwtN+AAAAcElEQVQY04XFMQrAMAgFUCGr4FUCXQO9uuAq/KsEugq2m2bqWx79kQRn1EzGyu' +
+          '1whx/Pc+gxKSnXku4ZNdFQolq2m3jN9/SrD0Ws9l4Ysx5uj9QftqstqQatmey2ftjW6GPI7PvD2iYE' +
+          'uJbEmlT/eAEXiXvHFX7hfQAAAABJRU5ErkJggg==';
+        const result = await BarCodeScanner.readFromURL(url);
+
+        t.expect(result).toBeDefined();
+        t.expect(result.length).toEqual(1);
+        t.expect(result[0]).toBeDefined();
+        t.expect(result[0].type).toEqual(BarCodeScanner.Constants.BarCodeType.qr);
+        t.expect(result[0].data).toEqual('test');
+      });
+
+      if (Platform.OS === 'android') {
+        t.it('reads a Data Matrix code from asset', async () => {
+          const asset = await Asset.fromModule(require('../assets/datamatrix_expo.png'));
+          const result = await BarCodeScanner.readFromURL(asset.uri);
+
+          t.expect(result).toBeDefined();
+          t.expect(result.length).toEqual(1);
+          t.expect(result[0]).toBeDefined();
+          t.expect(result[0].type).toEqual(BarCodeScanner.Constants.BarCodeType.datamatrix);
+          t.expect(result[0].data).toEqual('https://expo.io/');
+        });
+      }
+
+      t.it('respects barCodeTypes parameter', async () => {
+        const asset = await Asset.fromModule(require('../assets/datamatrix_expo.png'));
+        const result = await BarCodeScanner.readFromURL(asset.uri, [
+          BarCodeScanner.Constants.BarCodeType.qr,
+        ]);
+
+        t.expect(result).toBeDefined();
+        t.expect(result.length).toEqual(0);
+      });
+
+      t.it('works with multiple codes', async () => {
+        const asset = await Asset.fromModule(require('../assets/multiple_codes.png'));
+        const result = await BarCodeScanner.readFromURL(asset.uri);
+
+        t.expect(result).toBeDefined();
+        t.expect(result.length > 0).toBe(true);
       });
     });
   });
